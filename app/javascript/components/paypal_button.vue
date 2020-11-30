f9d8344ed97be4885e7f8724e613b7018e18d991
Galih Muhammad <galih0muhammad@gmail.com>
2020-11-30 08:46:28 +0700

    post/integrate-paypal-button-2
    Turn the paypal button into component

---
+++ app/javascript/components/paypal_button.vue
@@ -0,0 +1,55 @@
+<template>
+  <div :id="refer"></div>
+</template>
+
+<script>
+export default {
+  props: {
+    refer: {
+      type: String,
+      required: true
+    },
+    // Pass the product attributes to be used here
+  },
+  data() {
+    return {
+      // Defaults for the order that will be sent to Paypal
+      order: {
+        description: "",
+        amount: {
+          currency_code: "",
+          value: 0
+        }
+      }
+    };
+  },
+  mounted: function() {
+    // IMPORTANT: to cause the paypal button be loeaded and rendered
+    this.setLoaded();
+  },
+  computed: {
+    selectorContainer() {
+      return '#' + this.refer;
+    }
+  },
+  methods: {
+    setLoaded: function() {
+      paypal
+        .Buttons({
+          createOrder: (data, actions) => {
+            return actions.order.create({
+              purchase_units: [
+                this.order
+              ]
+            });
+          },
+          onApprove: async (data, actions) => {
+          },
+          onError: err => {
+            console.log(err);
+          }
+        }).render(this.selectorContainer);
+    }
+  }
+};
+</script>
--- app/javascript/packs/store.js
+++ app/javascript/packs/store.js
@@ -1,10 +1,13 @@
 import Vue from 'vue/dist/vue.esm'
+import { unpackSDKMeta } from '@paypal/sdk-client';
+import PaypalButton from '../components/paypal_button.vue';
 
 document.addEventListener('DOMContentLoaded', () => {
   var storeList = new Vue({
-    el: '', // Add the css identifier for the div that will load the paypal button
+    el: '#store-list', // Add the css identifier for the div that will load the paypal button
     components: {
       // Add paypal button here
+      PaypalButton
     }
   })
 })
