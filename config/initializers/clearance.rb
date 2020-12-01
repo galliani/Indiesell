247ded89c458abab043ce6d83f20e57327652175
Galih Muhammad <galih0muhammad@gmail.com>
2020-12-02 06:29:07 +0700

    Remove env vars on config for now

--- config/initializers/clearance.rb
+++ config/initializers/clearance.rb
@@ -1,7 +1,7 @@
 Clearance.configure do |config|
   config.routes                 = false
   config.allow_sign_up          = false
-  config.mailer_sender          = ENV['store_owner_email']
+  # config.mailer_sender          = ENV['store_owner_email']
   config.redirect_url           = '/admin'
   config.rotate_csrf_on_sign_in = true
   config.parent_controller      = '::Admin::ApplicationController' # IMPORTANT !!
