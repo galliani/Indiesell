a5d578791b22254eab2a3d8ee0854634a8626c50
Galih Muhammad <galih0muhammad@gmail.com>
2020-12-02 19:51:03 +0700

    Fix the annoying error caused by host env

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-Indiesell::Application.default_url_options = Indiesell::Application.config.action_mailer.default_url_options
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,11 +60,10 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "indiesell_production"
 
-  config.action_mailer.perform_caching = false
-
-  # Ignore bad email addresses and do not raise email delivery errors.
-  # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  # --- MAILER --- #
+  config.action_mailer.raise_delivery_errors  = false
+  config.action_mailer.perform_caching        = false
+  config.action_mailer.default_url_options    = { host: "http://#{ENV['store_host']}", port: ENV['store_port'] }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
