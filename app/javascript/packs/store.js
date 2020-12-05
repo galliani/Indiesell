9f5e6970c3f6dda2aec4f714cbd6b4487760c0d9
Galih Muhammad <galih0muhammad@gmail.com>
2020-12-05 11:28:41 +0700

    Experimenting for using made-component from npm instead

--- app/javascript/packs/store.js
+++ app/javascript/packs/store.js
@@ -1,12 +1,12 @@
 import Vue from 'vue/dist/vue.esm'
 import { unpackSDKMeta } from '@paypal/sdk-client';
-import PaypalButton from '../components/paypal_button.vue';
+import vuePaypalSmartPaymentButtons from "vue-paypal-smart-payment-buttons";
 
 document.addEventListener('DOMContentLoaded', () => {
   var storeList = new Vue({
     el: '#store-list',
     components: {
-      PaypalButton
+      vuePaypalSmartPaymentButtons
     },
     props: {
     },
