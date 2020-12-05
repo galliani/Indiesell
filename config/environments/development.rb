8430d4444523c80861315a9069e90ad339b75da9
Galih Muhammad <galih0muhammad@gmail.com>
2020-12-06 02:47:30 +0700

    Set default host for the url helper

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,6 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors  = false
   config.action_mailer.perform_caching        = false
   config.action_mailer.default_url_options    = { host: "http://#{ENV['store_host']}", port: ENV['store_port'] }
-  # Rails.application.routes.default_url_options[:host] = 'lvh.me'
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
